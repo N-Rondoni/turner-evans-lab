b8689532b7dabb0dfb411e7d76599100909602b4
Nick Rondoni <nrondoni@ucsc.edu>
2024-05-01 11:53:25 -0700

    Commit with freq response cooking, now actually finding periods after transients are done.

--- freqResponse/freqResponseTimeDomain.py
+++ freqResponse/freqResponseTimeDomain.py
@@ -8,25 +8,19 @@ import os
 import sys
 import time
 
-def impulseSine(t):
-    T = 3
-    A = 10
-    signal = A*np.abs(np.sin(np.pi*t/T))
-    return signal
 
+def sine(Amp, w, t):
+    """
+    Defines signal for which we will examine frequency response
 
-def impulseSaw(t):
-    # can only be called on a single element
-    A = 1
-    T = 5
-    if t % T < T/2:        
-        signal = 2*A*t/T
-    if t % T >= T/2:
-        signal = A - 2*A*t/T
-    return signal
+    Arguments: 
+        Amp : Amplitude
+        w   : Period
+        t   : time
+    """
+    out = Amp*np.sin(w*t) + 11
+    return out
 
-#def impulseSquare(t):
-#    T = 5
 
 
 def CRN(t, A):
@@ -38,18 +32,20 @@ def CRN(t, A):
                 A = [x, z]
         t : time
         p : vector of the parameters:
-                p = [kf, kr, alpha, gamma]
+                p = [kf, kr, amp, w]
+        s :  must be one of the impulse functions above, e.g., impulseSquare(t)
+             remember to change this in plotting functions below as well. 
     """
     x, z = A
-    #kf, kr, alpha, gamma, beta = p
-    kf, kr, alpha, gamma, L = p
+    kf, kr, amp, L, wNow = p # omega looped through in main
+    
+    s = sine(amp, wNow, t) + 11 # shift upwards
+    
 
     # define chemical master equation 
     # this is the RHS of the system     
-    
-    s = impulseSine(t)
-
-    du = [alpha*s - gamma*x + kr*z - kf*x*(L-z), # + beta 
+ 
+    du = [s - x + kr*z - kf*x*(L-z), # + beta 
         kf*x*(L-z) - kr*z] 
     
     return du
@@ -57,14 +53,11 @@ def CRN(t, A):
 
 if __name__=="__main__":
 
-    n = 1000
-    imRate = 11.4
-    tEnd = n*(1/imRate)
-    tEnd = 20
+    n = 2000
+    tEnd = 100
     timeVec = np.linspace(0, tEnd, n)
     tsolve = [0, tEnd]
-
-    print(tEnd)
+    transientTime = 49 #for small w, transients end after ~40s. 
 
     # define initial conditions
     X = 0 #Ca^{2+}
@@ -73,44 +66,91 @@ if __name__=="__main__":
     # define constants/reaction parameters, kr << kf
     kf = 0.0513514
     kr = 7.6
-    alpha = 1
-    gamma = 1    
+    amp = 10    
     # total sum of calcium indicator
     L = 100
     
-    # pack up parameters and ICs
-    p = [kf, kr, alpha, gamma, L]
+    # pack up parameters and ICs, looping through periods
     u0 = [X, Z]
 
-    start = time.time()
-    sol = solve_ivp(CRN, tsolve, u0, t_eval=timeVec)
-    end = time.time()
-
-    print('Total solve time ', end - start)
+    omegas = np.arange(0, 100, 0.1)
+    accumed_X = np.zeros((len(omegas), len(timeVec)))
+    accumed_Z = np.zeros((len(omegas), len(timeVec)))
+    accumed_S = np.zeros((len(omegas), len(timeVec)))
     
-    print(sol.y.shape)
-  
+    start = time.time()
+    
+    # simulate, stepping over w, while saving soln to other mat
+    for i in range(len(omegas)):
+        p = [kf, kr, amp, L, omegas[i]]
+        sol = solve_ivp(CRN, tsolve, u0, t_eval=timeVec)
+        accumed_X[i, :] = sol.y[0, :]
+        accumed_Z[i, :] = sol.y[1, :]
+        accumed_S[i, :] = sine(amp, omegas[i], timeVec)
+
+
+    end = time.time()
+    print('Total solve time ', end - start, "(s) for ", len(omegas), "different frequencies.")
+ 
+    # want to compute amplitude after ~ 10 seconds, as then transients have died down
+    transientIndex = len(timeVec) - len(np.where(timeVec > transientTime)[0])
+    # the above ensures timeVec[transientIndex] > 10
+
+    # for each w, compute amplitude
+    ampX = np.zeros(len(omegas))
+    ampZ = np.zeros(len(omegas))
+    ampS = np.zeros(len(omegas)) # not need but good sanity check, should always be A_in
+
+
+    for i in range(len(omegas)):
+        maxValX = np.max(accumed_X[i, transientIndex:])
+        minValX = np.min(accumed_X[i, transientIndex:])
+        ampX[i] = (maxValX - minValX)/2
+        
+        maxValZ = np.max(accumed_Z[i, transientIndex:])
+        minValZ = np.min(accumed_Z[i, transientIndex:])
+        ampZ[i] = (maxValZ - minValZ)/2
+        
+        maxValS = np.max(accumed_S[i, transientIndex:])
+        minValS = np.min(accumed_S[i, transientIndex:])
+        ampS[i] = (maxValS - minValS)/2
+ 
+   
     # plot signal, response in both state variables
     fig, axs = plt.subplots(1, 3)
-    # plots sawtooth impulse, use this format for things called on individal time moments.
-    #pulse = np.zeros(len(sol.t))
-    #for i in range(len(sol.t)):
-    #    pulse[i] = impulseSaw(sol.t[i])
-    #axs[0].plot(sol.t, pulse)
-    axs[0].plot(sol.t, impulseSine(sol.t)) # plots sine impulse
-    axs[0].set_xlabel(r'$t$', fontsize = 16)
-    axs[0].set_ylabel(r'Impulse', fontsize=16)
-    
-    axs[1].plot(sol.t, sol.y[0, :])
-    axs[1].set_xlabel(r'$t$', fontsize = 16)
-    axs[1].set_ylabel(r'$Ca^{2+}$', fontsize=16)
-    
-    axs[2].plot(sol.t, sol.y[1, :])
-    axs[2].set_xlabel(r'$t$', fontsize = 16)
-    axs[2].set_ylabel(r'$CI^*$', fontsize=16)
     
 
+#    for i in range(len(sol.t)):
+#        pulse[i] = impulseRamp(sol.t[i])
+#    axs[0].plot(sol.t, pulse)
 
+
+    # plot ratios of amplitudes
+#    axs[0].plot(omegas, ampS/ampX)
+#    axs[0].set_xlabel(r'Period $w$', fontsize = 16)
+#    axs[0].set_ylabel(r'$\frac{A(s)}{A(ca^{2+})}$', fontsize=16)
+    
+#    axs[1].plot(omegas, ampS/ampZ)
+#    axs[1].set_xlabel(r'Period $w$', fontsize = 16)
+#    axs[1].set_ylabel(r'$\frac{A(s)}{A(CI^*)}$', fontsize=16)
+#    plt.title("Frequency Response")
+    
+    # plot ratios of amplitudes
+    axs[0].plot(omegas, ampX/10)
+    axs[0].set_xlabel(r'$w$', fontsize = 16)
+    axs[0].set_ylabel(r'$\frac{A(ca^{2+})}{A(s)}$', fontsize=16)
+    axs[0].title.set_text("Calcium Ion")#, fontsize = 18)
+
+    axs[1].plot(omegas, ampZ/10)
+    axs[1].set_xlabel(r'$w$', fontsize = 16)
+    axs[1].set_ylabel(r'$\frac{A(CI^*)}{A(s)}$', fontsize=16)
+    axs[1].title.set_text("Calcium Indicator")#, fontsize = 18)
+
+    axs[2].plot(omegas, ampZ/10, label = r'$\frac{A(ca^{2+})}{A(s)}$')
+    axs[2].plot(omegas, ampX/10, label = r'$\frac{A(CI^*)}{A(s)}$')
+    axs[2].legend()
+    axs[2].title.set_text("Both Plots Overlaid")
+    axs[2].set_xlabel(r'$w$', fontsize = 16)
 
     plt.show()
 
